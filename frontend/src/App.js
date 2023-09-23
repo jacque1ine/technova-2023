620fd1b6b55f23c4a5fffe3666729f94065092cd
Jacqueline Fung <fungjsl@gmail.com>
2023-09-23 16:53:17 -0400

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,20 @@
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import { Box, Heading, Text } from '@chakra-ui/react';
-import { Box, Flex, Text, Link } from '@chakra-ui/react';
-import { css } from '@emotion/core';
+// src/App.js
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Route, Switch, Routes } from 'react-router-dom';
 import MyCard from './components/MyCard';
 import Tester from './components/Tester';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Box
-        css={css`
-          min-height: 100vh;
-        `}
-      >
-        <Flex
-          as="header"
-          align="center"
-          justify="space-between"
-          p={5}
-          bg="teal.500"
-          color="white"
-        >
-          <Text fontSize="xl">My Website</Text>
-          <Box>
-            <Link to="/" color="white">
-              Home
-            </Link>
-            <Link to="/about" color="white" ml={2}>
-              About
-            </Link>
-            <Link to="/contact" color="white" ml={2}>
-              Contact
-            </Link>
-          </Box>
-        </Flex>
-        <Route exact path="/" component={Tester} />
-        <Route path="/about" component={MyCard} />
-      </Box>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={MyCard} />
+          <Route path="/about" component={Tester} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
