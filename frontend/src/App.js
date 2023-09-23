00a9ca90387e96532becb8b706c14f5217fb5577
Jacqueline Fung <fungjsl@gmail.com>
2023-09-23 11:29:13 -0400

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import createProfile from "./components/createProfile";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
- 
+      <Router>
+        <Routes>
+          <Route path="/" element={<createProfile />} />
+        </Routes>
+      </Router>
+      <createProfile />
     </div>
   );
 }
