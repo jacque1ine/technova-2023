0cd03d0fecddf583fae6cf8a03a0988a0b5aee9d
Lorena Rosati <lrosati@outlook.com>
2023-09-23 22:46:30 -0400

    Styling changes for view challenges pg

--- frontend/src/components/ViewChallenges.js
+++ frontend/src/components/ViewChallenges.js
@@ -28,17 +28,17 @@ const ViewChallenges = (props) => {
 const Challenge = (props) => {
     return (
         <Center>
-            <Flex flexDirection='column' p={4}>
+            <Flex flexDirection='column' p={4} w="330px">
                     <Flex w="323px" h="73.6" flexDirection='row'>
                         <Box bg='tomato' p={5} pl={2} h="50px" w="50px" borderRadius="lg"></Box>
-                        <Flex flexDirection='column' pl={4}>
-                            <Flex flexDirection='row'>
+                        <Flex flexDirection='column' mx={4} w="full">
+                            <Flex flexDirection='row' justifyContent="space-between">
                             <Box align='left'>
                                 <Text fontSize="16px" fontWeight="bold">{props.title}</Text>
                                 <Text fontSize="13px">{props.timeleft}</Text>
                             </Box>
                             <Box pl={10} pt={1}>
-                                <Text fontSize="13px">{props.coins}</Text>
+                                <Text fontSize="13px">{props.coins} coins</Text>
                             </Box>
                             </Flex>
                             <Divider axis="horizontal" pt={5}/>
