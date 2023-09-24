032dc40e2d0a8fb4a6e1cc96dbe7a9fbd6e952dd
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2023-09-23 21:43:09 -0400

    Remove featured challenge from other challenge lists

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -5,9 +5,9 @@ const home = async(req, res, next) => {
   const userId = req.params.userId;
   const user = await usersCol.findOne({'_id': new ObjectId(userId)});
   const featuredChallenge = await challengesCol.findOne({'featured': true});
-  const myChallengesCursor = await challengesCol.find({'users.joined': userId});
-  const techChallengesCursor = await challengesCol.find({'type': 'tech'});
-  const hobbyChallengesCursor = await challengesCol.find({'type': 'hobby'});
+  const myChallengesCursor = await challengesCol.find({'users.joined': userId, 'featured': false});
+  const techChallengesCursor = await challengesCol.find({'type': 'tech', 'featured': false});
+  const hobbyChallengesCursor = await challengesCol.find({'type': 'hobby', 'featured': false});
 
   const myChallenges = await myChallengesCursor.toArray();
   const techChallenges = await techChallengesCursor.toArray();
