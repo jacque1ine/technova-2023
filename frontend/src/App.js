85ba7f6a0c94e4e8b406a9398bc25ee722300291
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2023-09-24 02:29:37 -0400

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { extendTheme } from '@chakra-ui/react';
+import CreateProfile from './components/CreateProfile';
+import MatchedSection from './components/MatchedSection';
+import ViewChallenge from './components/ViewChallenge';
+import ViewChallenges from './components/ViewChallenges';
 
 import '@fontsource/inter/700.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={null} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={null} />
+          <Route path='/create-profile' element={<CreateProfile />} />
+          <Route path='/match/*' element={<MatchedSection />} />
+          <Route path='/challenge/*' element={<ViewChallenge />} />
+          <Route path='/challenges/*' element={<ViewChallenges />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
